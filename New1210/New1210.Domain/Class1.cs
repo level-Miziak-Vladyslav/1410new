4b6c8dde952c4e4dab715aa88931335167e7e5f2
level-Miziak-Vladyslav <parado9921@gmail.com>
2018-10-13 17:09:10 +0300

    jhb

---
+++ New1210/New1210.Domain/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New1210.Domain
+{
+    public class Car
+    {
+        public string Name
+        {
+            get; set;
+        }
+        private double _price;
+        public Double Price
+        {
+            get
+            {
+                return _price;
+            }
+            set
+            {
+                _price = Deal(value);
+            }
+        }
+        double Deal(double _price)
+        {
+            _price = _price / 2;
+            return _price;
+        }
+    }
+    public class Service
+    {
+
+    }
+}
