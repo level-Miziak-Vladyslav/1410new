8446fbb4b3cd22cd23861f84b9966dcdad7e04c6
level-Miziak-Vladyslav <parado9921@gmail.com>
2018-10-17 13:27:39 +0300

    1710

--- New1210/New1210.Domain/Class1.cs
+++ New1210/New1210.Domain/Class1.cs
@@ -109,7 +109,11 @@ namespace New1210.Domain
     }
     #endregion
     #region Car
-    public class Car
+    public interface IStoring
+    {
+        double GetStoringTerm();
+    }
+    public class Car : IStoring
     {
         public string Name
         {
@@ -120,19 +124,59 @@ namespace New1210.Domain
         {
             get
             {
-                return _price;
+                return Deal(_price);
             }
             set
             {
-                _price = Deal(value);
+                _price = value;
             }
         }
-        double Deal(double _price)
+        public virtual double Deal(double _price)
         {
-            _price = _price / 2;
+            _price = _price * 0.8;
             return _price;
         }
+
+        public virtual double GetStoringTerm()
+        {
+
+            return Deal(_price);
+        }
     }
+    public class Minivan : Car, IStoring
+    {
+        public int Capacity
+        {
+            get; set;
+        }
+        public override double Deal(double _price)
+        {
+
+            //           if (DateTime.Now.Month )
+            _price = _price * 0.5;
+            return _price;
+        }
+
+    }
+    public class Truck : Car
+    {
+        public int Carrying
+        {
+            get; set;
+        }
+        public override double Deal(double _price)
+        {
+            _price = _price * 0.6;
+            return _price;
+        }
+
+        public override double GetStoringTerm()
+        {
+            return Deal(Price);
+        }
+    }
+
+
     public class CarsMain
     {
         public void MainRun()
@@ -143,54 +187,104 @@ namespace New1210.Domain
             Console.WriteLine("Please input price of car1");
             c1.Price = double.Parse(Console.ReadLine());
 
-
-            Car c2 = new Car();
+            var c2 = new Minivan();
             Console.WriteLine("Please input the name of car2");
             c2.Name = Console.ReadLine();
+            c2.Capacity = 7;
             Console.WriteLine("Please input price of car2");
             string s2 = Console.ReadLine();
             double i2;
             double.TryParse(s2, out i2);
             c2.Price = i2;
 
+            var c3 = new Truck();
+            Console.WriteLine("Please input the name of car2");
+            c3.Name = Console.ReadLine();
+            c3.Carrying = 16;
+            Console.WriteLine("Please input price of car2");
+            string s3 = Console.ReadLine();
+            double i3;
+            double.TryParse(s3, out i3);
+            c3.Price = i3;
+            Car[] cars = new Car[3];
+            cars[0] = c1;
+            cars[1] = c2;
+            cars[2] = c3;
+            if (c1 is IStoring stored)
+            {
+                c1.Price = 1000;
+            }
+            if (c2 is IStoring stored1)
+            {
+                c2.Price = 1000;
+            }
+            if (c3 is IStoring stored2)
+            {
+                c3.Price = 1000;
+            }
+
+
+            //            foreach c in cars { }
             Console.WriteLine("Name of car 1 {0}, Price car 1 {1}", c1.Name, c1.Price);
             Console.WriteLine("Name of car 2 {0}, Price car 2 {1}", c2.Name, c2.Price);
+            Console.WriteLine("Name of car 3 {0}, Price car 3 {1}", c3.Name, c3.Price);
         }
     }
     #endregion
     #region ITSchool
     public class ITSchool
     {
-        public static int Qp = 0;
+        static int Qp = 0;
+        static string[,] BasePeople;
         public void MainRun()
         {
             Qp = 0;
             int s = 0;
-            int d = 0;
             int dc = 0;
             int l = 0;
             int a = 0;
 
-            string[,] BasePeople = new string[1000, 2];
+            BasePeople = new string[1000, 3];
             Console.WriteLine("Please input total number of student");
             s = int.Parse(Console.ReadLine());
-            Console.WriteLine("Please input total number of Ph.D");
-            d = int.Parse(Console.ReadLine());
             Console.WriteLine("Please input total number of professor assistant");
             dc = int.Parse(Console.ReadLine());
             Console.WriteLine("Please input total number of lecturer");
             l = int.Parse(Console.ReadLine());
             Console.WriteLine("Please input total number of assistant");
             a = int.Parse(Console.ReadLine());
-            BasePeople = MakePerson(s,d,dc,l,a, BasePeople);
+            MakePerson(s, dc, l, a);
+            Console.WriteLine("List of person. Name // Role");
             DrawMatrix(BasePeople);
-
+            int fs = s;
+            int fh = (dc + l + a);
+            int i = 0;
+            ITGroup[] g = new ITGroup[fh];
+            while (fs > 0)
+            {
+                if (i == fh)
+                {
+                    Console.WriteLine("No free teachers, students without group: {}",fs);
+                    break;
+                }
+                g[i] = new ITGroup(BasePeople[(Qp-i-1),1]);
+                int rs = s - fs;
+                for (int p = rs; p < (g[i].Qs + rs); p++)
+                {
+                     g[i].sl[p - rs] = BasePeople[p, 0];
+                    BasePeople[p, 2] = g[i].HeadName;
+                }
+                fs = fs - g[i].Qs;
+                i++;
+            }
+            Console.WriteLine("");
+            DrawMatrix(BasePeople);
         }
         public static void DrawMatrix(string[,] matrix)
         {
             for (int i = 0; i < Qp; i++)
             {
-                for (int j = 0; j < 2; j++)
+                for (int j = 0; j < 3; j++)
                 {
                     Console.Write($"{(matrix[i, j].ToString().Length < 2 ? matrix[i, j].ToString() + "  |" : matrix[i, j].ToString() + " |")}");
                 }
@@ -198,49 +292,77 @@ namespace New1210.Domain
             }
         }
 
-        string[,] MakePerson(int s, int d, int dc, int l, int a, string[,] BasePeople)
+        void MakePerson(int s, int dc, int l, int a)
         {
-            for (int i = 0; i < (s+d+dc+l+a); i++)
+            for (int i = 0; i < (s + dc + l + a); i++)
             {
-                if(Qp < s)
+                if (Qp < s)
                 {
                     BasePeople[i, 0] = "Student # " + (Qp + 1).ToString();
                     BasePeople[i, 1] = "Student";
+                    BasePeople[i, 2] = "0";
                 }
-                else if(Qp < s + d)
-                {
-                    BasePeople[i, 0] = "Ph.D # " + (Qp + 1).ToString();
-                    BasePeople[i, 1] = "Ph.D";
-                }
-                else if (Qp < s + d + dc)
+                else if (Qp < s + dc)
                 {
                     BasePeople[i, 0] = "professor assistant # " + (Qp + 1).ToString();
                     BasePeople[i, 1] = "professor assistant";
+                    BasePeople[i, 2] = "0";
                 }
-                else if (Qp < s + d + dc + l)
+                else if (Qp < s + dc + l)
                 {
                     BasePeople[i, 0] = "lecturer # " + (Qp + 1).ToString();
                     BasePeople[i, 1] = "lecturer";
+                    BasePeople[i, 2] = "0";
                 }
-                else 
+                else
                 {
                     BasePeople[i, 0] = "assistant # " + (Qp + 1).ToString();
                     BasePeople[i, 1] = "assistant";
+                    BasePeople[i, 2] = "0";
                 }
 
                 Qp++;
             }
-            return BasePeople;
-        }        
+        }
+        struct ITGroup
+        {
+            public string[] sl;
+            public string HeadName;
+            public int Qs;
+            public ITGroup(string hn)
+            {
+                HeadName = hn;
+                for (int i = 0; i < Qp; i++)
+                {
+                    if (BasePeople[i, 0] == hn)
+                    {
+                        string HeadType = BasePeople[i, 1];
+                    }
+                }
+                if (hn == "professor assistant")
+                {
+                    Qs = 20;
+                }
+                else if (hn == "lecturer")
+                {
+                    Qs = 15;
+                }
+                else
+                {
+                    Qs = 5;
+                }
+                sl = new string[Qs];
+            }
+        }
     }
 
-    
 
-    
 
-    
 
-    
+
+
+
+
     #endregion
     public class Service
     {
